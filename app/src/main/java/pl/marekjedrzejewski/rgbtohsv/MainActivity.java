0a88d22dd9c9638f0e9590ff062285db8cb477bf
marek <mjedrzejewski92@gmail.com>
2016-03-20 12:56:46 +0100

    Remove unecessary imports

--- app/src/main/java/pl/marekjedrzejewski/rgbtohsv/MainActivity.java
+++ app/src/main/java/pl/marekjedrzejewski/rgbtohsv/MainActivity.java
@@ -1,11 +1,9 @@
 package pl.marekjedrzejewski.rgbtohsv;
 
-import android.content.res.Configuration;
 import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.InputType;
-import android.view.View;
 import android.widget.EditText;
 import android.widget.NumberPicker;
 import android.widget.TextView;
@@ -62,7 +60,7 @@ public class MainActivity extends AppCompatActivity {
             floatstrings[i] = String.valueOf((float)(i) / 100.f);
         }
         numberPicker.setDisplayedValues(floatstrings);
-        
+
         // wyświetli klawiaturę numeryczną zamiast tekstowej
         ((EditText)numberPicker.getChildAt(0)).setRawInputType(InputType.TYPE_CLASS_NUMBER);
 
